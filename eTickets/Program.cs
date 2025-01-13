1e95013841ac891b9fde02e7f7fb2cade1c92cd1
Huy Tran <tranhuy28102002@gmail.com>
2025-01-14 01:22:31 +0700

    connect-sqlserver

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,10 +1,14 @@
 using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
