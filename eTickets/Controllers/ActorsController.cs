3f15617d0fe49ea0e138574f0c276e434d5f8ddf
Huy Tran <tranhuy28102002@gmail.com>
2025-01-16 00:37:00 +0700

    create controller class and view for each controller.

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using eTickets.Data;
+using eTickets.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Actor> actors = _context.Actors.ToList();
+            return View(actors);
+        }
+    }
+}
---
+++ eTickets/Controllers/CinemasController.cs
@@ -0,0 +1,20 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        public readonly AppDbContext _context;
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Cinemas.ToListAsync();
+            return View(data);
+        }
+    }
+}
