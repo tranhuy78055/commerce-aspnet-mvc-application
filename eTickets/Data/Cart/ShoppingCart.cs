807fcb0b739129aedf41739b086b54e94f250350
Huy Tran <tranhuy28102002@gmail.com>
2025-02-09 00:11:04 +0700

    completed cart ,orders and login

---
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,83 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Cart
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _context;
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string ShoppingCartId { get; set; } = string.Empty;
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; } = new List<ShoppingCartItem>();
+
+        public static ShoppingCart GetShoppingCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            var context = services.GetService<AppDbContext>();
+
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddItemToCart(Movie movie)
+        {
+            var shoppingCartItem = _context.Set<ShoppingCartItem>().FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Movie = movie,
+                    Amount = 1
+                };
+
+                _context.Set<ShoppingCartItem>().Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _context.SaveChanges();
+        }
+
+        public void RemoveItemFromCart(Movie movie)
+        {
+            var shoppingCartItem = _context.Set<ShoppingCartItem>().FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                }
+                else
+                {
+                    _context.Set<ShoppingCartItem>().Remove(shoppingCartItem);
+                }
+            }
+            _context.SaveChanges();
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.Set<ShoppingCartItem>().Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal() => _context.Set<ShoppingCartItem>().Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Movie.Price * n.Amount).Sum();
+
+        public async Task ClearShoppingCartAsync()
+        {
+            var items = await _context.Set<ShoppingCartItem>().Where(n => n.ShoppingCartId == ShoppingCartId).ToListAsync();
+            _context.Set<ShoppingCartItem>().RemoveRange(items);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,5 +1,9 @@
 using eTickets.Data;
+using eTickets.Data.Cart;
 using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +17,23 @@ builder.Services.AddScoped<IActorsServices, ActorsService>();
 builder.Services.AddScoped<IProducersService, ProducersService>();
 builder.Services.AddScoped<ICinemasService,CinemasService > ();
 builder.Services.AddScoped<IMoviesService, MoviesService>();
+builder.Services.AddScoped<IOrdersService, OrdersService>();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+builder.Services.AddSession();
+
+
+// Authentication and authorization
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,13 +48,18 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
+//Authentication and authorization
+app.UseAuthentication();
 app.UseAuthorization();
-AppDbInitializer.Seed(app);
 
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed database
+AppDbInitializer.Seed(app);
+AppDbInitializer.SeedUsersAndRolesAsync(app).Wait();
 
 app.Run();
